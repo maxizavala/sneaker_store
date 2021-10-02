61c1432f815aafa4dff932fc1a808886c2387862
maxizavala <maximiliano.zavala@davinci.edu.ar>
2021-10-02 00:46:30 -0300

    setTimeot

--- src/components/container/ItemList.js
+++ src/components/container/ItemList.js
@@ -1,22 +1,44 @@
+import React, {useState,useEffect} from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Item from "./Item";
 
-const ItemList = (props) => {
+const articulos = ["zapatilla1", "zapatilla2", "zapatilla3"]
 
-    const articulos = ["zapatilla1", "zapatilla2", "zapatilla3"]
+function crearPromesa() {
+    return new Promise((resolve, reject) => {  
+        setTimeout(
+            function(){                          
+                resolve(articulos);  
+        }, 2000);      
+    });     
+}
+
+
+const ItemList = (props) => {
+    const [articulos, setArticulos] = React.useState(null);
+    const [estado, setEstado] = useState('Loading...');
+
+    useEffect(() => {        
+        let requestArticulos = crearPromesa();
+
+        requestArticulos
+        .then( function(items_promise){
+            setArticulos(items_promise);     
+            setEstado('Listo!');           
+        })      
+    }, []);
 
     return (
-            <Container>
-
-                <Row>
-                    {articulos.map(u => <Col sm={6} md={4} className="pt-3"> <Item titulo={u}/> </Col>)}
-                </Row>
-
-            </Container>
+        <Container>
+            <Row>
+                {(articulos !== null) && 
+                articulos.map(prod => <Col sm={6} md={4} className="pt-3"> <Item titulo={prod}/> </Col>)}
+            </Row>
+        </Container>
     );
-    
+ 
 }
 
 export default ItemList
--- src/components/container/ItemListContainer.js
+++ src/components/container/ItemListContainer.js
@@ -2,11 +2,6 @@ import React, { useState, useEffect } from "react";
 import ItemList from './ItemList';
 
 const ItemListContainer = () => {
-  
-    setTimeout(() => {
-        console.log('Esto se ejecuta despues de 2 segundos')
-    }, 2000);
-
 
     return(
         <div className="container">
