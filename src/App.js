0fb8e0fcd4419bf07414c0a1d6a434eae02c6fb0
maxizavala <maximiliano.zavala@davinci.edu.ar>
2021-10-04 10:18:45 -0300

    Correccion 3er desafio:  MAP y Promises

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// Importo los componentes
 import NavBar from "./components/NavBar"
 import Main from "./components/Main"
+import { BrowserRouter } from "react-router-dom"
 
 const App = () => {
 
     return(
-        <>
+        <BrowserRouter>
             <NavBar titulo="SNEAKER STORE"/>
             <Main/>
-        </>
+        </BrowserRouter>
     )
     
 }
--- src/components/container/ItemList.js
+++ src/components/container/ItemList.js
@@ -1,38 +1,11 @@
-import React, {useState,useEffect} from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Item from "./Item";
 
-const articulos = [
-    {id:1, nombre: "Nike M2K Tekno", img: "https://www.moovbydexter.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-dabra-catalog/default/dweb3a9dc7/products/NI_AO3108-401/NI_AO3108-401-1.JPG?sw=400&sh=400", precio: "$7.800,00"}, 
-    {id:2, nombre: "Nike Air Max 200", img: "https://www.moovbydexter.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-dabra-catalog/default/dwaa5211b9/products/NI_AT6175-100/NI_AT6175-100-1.JPG?sw=400&sh=400", precio: "$8.100,00"}, 
-    {id:3, nombre: "Nike Air Max 270", img: "https://www.moovbydexter.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-dabra-catalog/default/dw3b12c7eb/products/NI_AH6789-107/NI_AH6789-107-1.JPG?sw=400&sh=400", precio: "$7.900,00"}
-]
+const ItemList = (props) => {
 
-function crearPromesa() {
-    return new Promise((resolve) => {  
-        setTimeout(
-            function(){                          
-                resolve(articulos);  
-        }, 2000);      
-    });     
-}
-
-
-const ItemList = () => {
-    const [articulos, setArticulos] = React.useState(null);
-    const [estado, setEstado] = useState('Cargando');
-
-    useEffect(() => {        
-        let requestArticulos = crearPromesa();
-
-        requestArticulos
-        .then( function(items_promise){
-            setArticulos(items_promise);     
-            setEstado('Listo');           
-        })      
-    }, []);
+    const articulos = props.productos
 
     return (
         <Container>
